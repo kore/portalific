f8085ac932107ca57b67d63225bc91430ecea179
Kore Nordmann <mail@kore-nordmann.de>
2022-08-06 07:15:48 +0200

    Reduced whitesapce for ehader and footer to have more screen estate for content

--- components/Footer.js
+++ components/Footer.js
@@ -49,7 +49,7 @@ const moonIcon = (
 
 const ThemeSwitcher = () => {
   return (
-    <div className="flex mt-6 bg-white justify-center dark:bg-gray-900 rounded-3xl p-1">
+    <div className="flex ml-4 bg-white justify-center dark:bg-gray-900 rounded-3xl p-1">
       <button
         type="button"
         aria-label="Use Dark Mode"
@@ -57,7 +57,7 @@ const ThemeSwitcher = () => {
           document.documentElement.classList.add('dark');
           localStorage.setItem('theme', 'dark');
         }}
-        className="flex items-center h-full pr-2 dark:bg-primary rounded-3xl flex justify-center align-center p-2 w-24 h-10 transition"
+        className="flex items-center h-full dark:bg-primary rounded-3xl flex justify-center align-center p-1 w-16 h-8 transition"
       >
         {moonIcon}
       </button>
@@ -69,7 +69,7 @@ const ThemeSwitcher = () => {
           document.documentElement.classList.remove('dark');
           localStorage.setItem('theme', 'light');
         }}
-        className="flex items-center h-full pr-2 bg-primary dark:bg-transparent rounded-3xl flex justify-center align-center p-2 w-24 h-10 transition"
+        className="flex items-center h-full bg-primary dark:bg-transparent rounded-3xl flex justify-center align-center p-1 w-16 h-8 transition"
       >
         {sunIcon}
       </button>
@@ -79,8 +79,8 @@ const ThemeSwitcher = () => {
 
 export default function Footer({ copyrightText }) {
   return (
-    <footer className="py-16 flex flex-col items-center">
-      <p className="dark:text-white uppercase mb-3 font-bold opacity-60">
+    <footer className="py-16 flex flex-row items-center">
+      <p className="dark:text-white uppercase mb-1 font-bold opacity-60">
         {copyrightText}
       </p>
       <ThemeSwitcher />
