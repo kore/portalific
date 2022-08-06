9161faa5e29040d5dc8dc7e3e9e864224af89423
Kore Nordmann <mail@kore-nordmann.de>
2022-08-06 07:49:02 +0200

    Stripped down themes

--- themes.js
+++ themes.js
@@ -8,42 +8,6 @@ exports.COLOR_THEMES = {
       'gradient-4': '#8785FF',
     },
   },
-  bejamas: {
-    colors: {
-      primary: '#FF8585',
-      'gradient-1': '#7d7aff',
-      'gradient-2': '#2121E2',
-      'gradient-3': '#FF76B8',
-      'gradient-4': '#001AFF',
-    },
-  },
-  netlify: {
-    colors: {
-      primary: '#00A354',
-      'gradient-1': '#00F0FF',
-      'gradient-2': '#00F0FF',
-      'gradient-3': '#FAFF00',
-      'gradient-4': '#00F0FF',
-    },
-  },
-  reddie: {
-    colors: {
-      primary: '#FF4D4D',
-      'gradient-1': '#FFC700',
-      'gradient-2': '#FF85DD',
-      'gradient-3': '#FF85DD',
-      'gradient-4': '#FF8585',
-    },
-  },
-  greenie: {
-    colors: {
-      primary: '#C78500',
-      'gradient-1': '#FFCC81',
-      'gradient-2': '#00F37F',
-      'gradient-3': '#00F37F',
-      'gradient-4': '#FFCC81',
-    },
-  },
 };
 
 exports.FONT_THEMES = {
