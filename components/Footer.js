37ea71f8f17e2db6c88b1242b8fcde06cf644d84
Kore Nordmann <mail@kore-nordmann.de>
2022-08-13 09:55:28 +0200

    feat: Implemented time based automatic theme selection

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,31 @@
+import { useState, useEffect } from "react";
 import { SunIcon, MoonIcon, GlobeIcon } from "@heroicons/react/outline";
 
 const ThemeSwitcher = () => {
+  const [auto, setAuto] = useState(true);
+  const [time, setTime] = useState(new Date().getTime());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (!auto) {
+        return;
+      }
+
+      const hours = new Date().getHours();
+      if (hours > 6 && hours < 20) {
+        document.documentElement.classList.remove("dark");
+      } else {
+        document.documentElement.classList.add("dark");
+      }
+
+      setTime(hours);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [auto]);
+
   return (
     <div className="ml-4 flex justify-center rounded-3xl bg-white p-1 dark:bg-gray-900">
       <button
@@ -9,8 +34,9 @@ const ThemeSwitcher = () => {
         onClick={() => {
           document.documentElement.classList.add("dark");
           localStorage.setItem("theme", "dark");
+          setAuto(false);
         }}
-        className="flex h-8 w-16 items-center justify-center rounded-3xl p-1 text-center transition dark:bg-primary-500"
+        className="flex h-8 w-12 items-center justify-center rounded-3xl p-1 text-center transition dark:bg-primary-500"
       >
         <MoonIcon className="h-6 w-6 text-gray-300 dark:text-white" aria-hidden="true" />
       </button>
@@ -21,10 +47,23 @@ const ThemeSwitcher = () => {
         onClick={() => {
           document.documentElement.classList.remove("dark");
           localStorage.setItem("theme", "light");
+          setAuto(false);
         }}
-        className="flex h-8 w-16 items-center justify-center rounded-3xl bg-primary-500 p-1 text-center transition dark:bg-transparent"
+        className="flex ml-2 h-8 w-12 items-center justify-center rounded-3xl bg-primary-500 p-1 text-center transition dark:bg-transparent"
       >
         <SunIcon className="h-6 w-6 text-gray-100 dark:text-gray-400" aria-hidden="true" />
+      </button>
+
+      <button
+        type="button"
+        aria-label="Use Auto Mode"
+        onClick={() => {
+          localStorage.setItem("theme", "auto");
+          setAuto(true);
+        }}
+        className={"flex ml-2 h-8 w-12 items-center justify-center rounded-3xl p-1 text-center transition " + (auto ? "bg-primary-700" : "bg-gray-100 dark:bg-gray-800")}
+      >
+        <GlobeIcon className={"h-6 w-6 " + (auto ? "text-white" : "text-gray-700")} aria-hidden="true" />
       </button>
     </div>
   );
