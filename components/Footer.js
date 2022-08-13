6dc754701675992d9a7e3f6d8dd4bfe95492cd60
Kore Nordmann <mail@kore-nordmann.de>
2022-08-13 09:26:03 +0200

    Use heroicons instead of custom SVG

--- components/Footer.js
+++ components/Footer.js
@@ -1,51 +1,4 @@
-const sunIcon = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="25"
-    height="24"
-    fill="none"
-    viewBox="0 0 25 24"
-    className="dark:opacity-50"
-  >
-    <g
-      stroke="#fff"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="2"
-      clipPath="url(#clip0_192_823)"
-    >
-      <path d="M12.5 17a5 5 0 100-10 5 5 0 000 10zM12.5 1v2M12.5 21v2M4.72 4.22l1.42 1.42M18.86 18.36l1.42 1.42M1.5 12h2M21.5 12h2M4.72 19.78l1.42-1.42M18.86 5.64l1.42-1.42"></path>
-    </g>
-    <defs>
-      <clipPath id="clip0_192_823">
-        <path
-          className="fill-current text-white"
-          d="M0 0H24V24H0z"
-          transform="translate(.5)"
-        ></path>
-      </clipPath>
-    </defs>
-  </svg>
-);
-
-const moonIcon = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="21"
-    height="20"
-    fill="none"
-    viewBox="0 0 21 20"
-  >
-    <path
-      stroke="#fff"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="2"
-      className="stroke-current text-gray-400 dark:text-white"
-      d="M19.5 10.79A9 9 0 119.71 1a7 7 0 009.79 9.79v0z"
-    ></path>
-  </svg>
-);
+import { SunIcon, MoonIcon, GlobeIcon } from "@heroicons/react/outline";
 
 const ThemeSwitcher = () => {
   return (
@@ -59,7 +12,7 @@ const ThemeSwitcher = () => {
         }}
         className="flex h-8 w-16 items-center justify-center rounded-3xl p-1 text-center transition dark:bg-primary-500"
       >
-        {moonIcon}
+        <MoonIcon className="h-6 w-6 text-gray-300 dark:text-white" aria-hidden="true" />
       </button>
 
       <button
@@ -71,7 +24,7 @@ const ThemeSwitcher = () => {
         }}
         className="flex h-8 w-16 items-center justify-center rounded-3xl bg-primary-500 p-1 text-center transition dark:bg-transparent"
       >
-        {sunIcon}
+        <SunIcon className="h-6 w-6 text-gray-100 dark:text-gray-400" aria-hidden="true" />
       </button>
     </div>
   );
