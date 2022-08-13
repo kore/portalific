d2968dc4e6d49004fd5f6be9a135ff7d0ebe10f7
Kore Nordmann <mail@kore-nordmann.de>
2022-08-13 20:53:34 +0200

    fix: Fetch calendars even one fails

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Link from "next/link";
-import { CogIcon } from "@heroicons/react/outline";
+import { CogIcon, ExclamationIcon } from "@heroicons/react/outline";
 import Modal from "./Modal";
 import Settings from "./Settings";
 
@@ -11,14 +11,62 @@ export default function Header({
   moveModule,
   settings,
   setSettings,
+  errors,
+  clearErrors,
 }) {
   const [showSettings, setShowSettings] = useState(false);
+  const [showErrors, setShowErrors] = useState(false);
 
   return (
     <header className="flex w-full items-center p-2">
       <Link href="/">
         <a className="grow text-left text-2xl dark:text-white">{name}</a>
       </Link>
+      {errors && !!errors.length && <button
+        type="button"
+        className="ml-1 shrink-0 rounded-full p-1 text-red-200 hover:bg-red-800 hover:text-white focus:bg-red-900 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-red-900 animate-pulse"
+        onClick={() => setShowErrors(true)}
+      >
+        <span className="sr-only">View notifications</span>
+        <ExclamationIcon className="h-6 w-6" aria-hidden="true" />
+      </button>}
+      <Modal open={showErrors} setOpen={setShowErrors}>
+        <ul role="list" className="">
+          {errors.map((error, index) => (
+            <li key={index}>
+              <div className="relative pb-8">
+                {index !== errors.length - 1 ? (
+                  <span className="absolute top-4 left-4 -ml-px h-full w-0.5 bg-gray-200" aria-hidden="true" />
+                ) : null}
+                <div className="relative flex space-x-3">
+                  <div>
+                    <span className='h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-white bg-red-500'>
+                      <ExclamationIcon className="h-5 w-5 text-white" aria-hidden="true" />
+                    </span>
+                  </div>
+                  <div className="min-w-0 flex-1 pt-1.5 flex justify-between space-x-4">
+                    <div>
+                      <p className="text-md">
+                        {error.error}
+                      </p>
+                      {error.info && <div className="text-sm whitespace-nowrap text-gray-500">
+                        {error.info}
+                      </div>}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+        <button
+          type="button"
+          className="inline-flex justify-center rounded-md border border-transparent px-4 py-2 text-base font-medium shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 sm:text-sm"
+          onClick={() => {clearErrors(); setShowErrors(false); }}
+        >
+          Clear all errors
+        </button>
+      </Modal>
       <button
         type="button"
         className="ml-1 shrink-0 rounded-full p-1 text-primary-200 hover:bg-primary-800 hover:text-white focus:bg-primary-900 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-primary-900"
--- modules/Calendar/index.js
+++ modules/Calendar/index.js
@@ -16,96 +16,105 @@ const groupBy = (list, keyGetter) => {
   return map;
 };
 
-export default function Calendar({ configuration, updateModuleConfiguration }) {
+export default function Calendar({ configuration, updateModuleConfiguration, pushError }) {
   const [calendarItems, setCalendarItems] = useState([]);
 
   // Use https://stackoverflow.com/questions/72540660/react-how-to-combine-data-from-multiple-api-and-render-it to fetch data from all feeds
   const updateCalendars = () => {
-    Promise.all(
+    Promise.allSettled(
       (configuration.calendars ?? []).map(async (calendar) =>
         axios.get(
           "https://k023.de/allowProxy.php?url=" +
             encodeURIComponent(calendar.calendar),
-          { color: calendar.color, name: calendar.name }
+          { color: calendar.color, name: calendar.name, calendarUrl: calendar.calendar }
         )
       )
-    ).then(
-      axios.spread((...iCalendarDataSets) => {
-        const allAppointments = iCalendarDataSets.map((iCalendarData) => {
-          const jcalData = ICAL.parse(iCalendarData.data);
-          const vCalendar = new ICAL.Component(jcalData);
-          const calendarStart = new Date();
-          const calendarEnd = new Date();
-          calendarEnd.setDate(calendarStart.getDate() + 7);
+    ).then((calendarResponses) => {
+      const successfulResponses = calendarResponses.map((response) => {
+        if (response.status === 'rejected') {
+          pushError(
+            response.reason.message,
+            `Calendar: ${response.reason.config.name}, URL: ${response.reason.config.calendarUrl}`
+          );
 
-          const appointments = [];
-          vCalendar.getAllSubcomponents("vevent").map((vEvent) => {
-            const id = vEvent.getFirstPropertyValue("uid");
-            const recuring = vEvent.getFirstPropertyValue("rrule");
-            const start = new Date(vEvent.getFirstPropertyValue("dtstart"));
-            const end = new Date(vEvent.getFirstPropertyValue("dtend"));
-            const summary = vEvent.getFirstPropertyValue("summary");
+          return null;
+        }
 
-            if (recuring) {
-              const iterator = recuring.iterator(
-                vEvent.getFirstPropertyValue("dtstart")
+        return response.value;
+      }).filter((item) => !!item);
+
+      const allAppointments = successfulResponses.map((iCalendarData) => {
+        const jcalData = ICAL.parse(iCalendarData.data);
+        const vCalendar = new ICAL.Component(jcalData);
+        const calendarStart = new Date();
+        const calendarEnd = new Date();
+        calendarEnd.setDate(calendarStart.getDate() + 7);
+
+        const appointments = [];
+        vCalendar.getAllSubcomponents("vevent").map((vEvent) => {
+          const id = vEvent.getFirstPropertyValue("uid");
+          const recuring = vEvent.getFirstPropertyValue("rrule");
+          const start = new Date(vEvent.getFirstPropertyValue("dtstart"));
+          const end = new Date(vEvent.getFirstPropertyValue("dtend"));
+          const summary = vEvent.getFirstPropertyValue("summary");
+
+          if (recuring) {
+            const iterator = recuring.iterator(
+              vEvent.getFirstPropertyValue("dtstart")
+            );
+
+            let date, endDate;
+            do {
+              date = iterator.next();
+              if (!date) {
+                break;
+              }
+
+              date = date.toJSDate();
+              endDate = new Date().setTime(
+                date.getTime() + (end.getTime() - start.getTime())
               );
 
-              let date, endDate;
-              do {
-                date = iterator.next();
-                if (!date) {
-                  break;
-                }
-
-                date = date.toJSDate();
-                endDate = new Date().setTime(
-                  date.getTime() + (end.getTime() - start.getTime())
-                );
-
-                if (endDate > calendarStart && date < calendarEnd) {
-                  appointments.push({
-                    id,
-                    summary,
-                    start: date,
-                    end,
-                    color: iCalendarData.config.color,
-                    calendar: iCalendarData.config.name,
-                  });
-                }
-              } while (date < calendarEnd);
-              return;
-            } else {
-              if (end < calendarStart || start > calendarEnd) {
-                return;
+              if (endDate > calendarStart && date < calendarEnd) {
+                appointments.push({
+                  id,
+                  summary,
+                  start: date,
+                  end,
+                  color: iCalendarData.config.color,
+                  calendar: iCalendarData.config.name,
+                });
               }
+            } while (date < calendarEnd);
+            return;
+          } else {
+            if (end < calendarStart || start > calendarEnd) {
+              return;
             }
+          }
 
-            appointments.push({
-              id,
-              summary,
-              start,
-              end,
-              color: iCalendarData.config.color,
-              calendar: iCalendarData.config.name,
-            });
+          appointments.push({
+            id,
+            summary,
+            start,
+            end,
+            color: iCalendarData.config.color,
+            calendar: iCalendarData.config.name,
           });
-
-          return appointments;
         });
 
-        const appointments = [].concat.apply([], allAppointments);
-        appointments.sort((a, b) => a.start - b.start);
-        setCalendarItems(appointments);
-      })
-    );
-
-    /*
-      ).then((result) => {
-        return result.text();
-      }).then((iCalendarData) => {
+        return appointments;
       });
-    */
+
+      const appointments = [].concat.apply([], allAppointments);
+      appointments.sort((a, b) => a.start - b.start);
+      setCalendarItems(appointments);
+    }).catch((error) => {
+      pushError(
+        error.message,
+        `Calendar: ${error.config.name}, URL: ${error.config.calendarUrl}`
+      );
+    });
   };
 
   useEffect(() => {
