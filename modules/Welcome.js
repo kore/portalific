1a47a7172dd86617f1434772f40420a628a234e4
Kore Nordmann <mail@kore-nordmann.de>
2022-08-14 18:07:49 +0200

    Added privacy section to welcome module

--- modules/Welcome.js
+++ modules/Welcome.js
@@ -14,9 +14,9 @@ export default function Welcome() {
       <p className="mt-8 text-xl text-gray-500 leading-8">
         Torii is an OpenSource browser based portal, displaying your feeds,
         your calendars, and other information of personal relevance. It works
-        (almost) entirely browser-based, so you data is safe and secure. For
-        cross-device synchronization you can optionally use a backend to
-        synchronize the data continuously between devices.
+        (almost<a href="#privacy">*</a>) entirely browser-based, so you data is
+        safe and secure. For cross-device synchronization you can optionally
+        use a backend to synchronize the data continuously between devices.
       </p>
       <h2>From beginner to expert in 30 days</h2>
       <p>
@@ -42,6 +42,31 @@ export default function Welcome() {
           pellentesque. Blandit amet, sed aenean erat arcu morbi.
         </p>
       </blockquote>
+      <h2 id="privacy">Privacy</h2>
+      <p>
+        All data is, by default, stored only in your browser, in <a
+        href="https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage">Local
+        Storage</a>.
+      </p>
+      <p>
+        Browsers don't allo to request data from foreign domains, though, <a
+        href="https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS">unless
+        some very specific headers are set</a>. This is why requesting feeds or
+        calendar data has to go through a server-side proxy. This proxy, in
+        theory, would allow to log all your calendar and feed contents. The
+        used implementation does not log anything like this, and <a
+        href="https://github.com/kore/torii2/blob/main/docs/allowProxy.php">can
+        be found in the code repository</a>. If you want to be super safe, you
+        should host Torii yourself and deploy your own allow-proxy.
+      </p>
+      <p>
+        Optionally we allow for cross-device synchronozation of your Torii
+        configuration. This will require your configuration to be stored on a
+        server. You must explicitly enable this, since it is
+        <strong> not</strong> enabled by default. Your configuration will be
+        stored encrypted even then, so nobody will be able to spy on your
+        configuration.
+      </p>
     </div>
   );
 }
