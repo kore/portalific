924085bb981a69769ba652c627c81741a70601e9
Kore Nordmann <mail@kore-nordmann.de>
2022-08-14 18:08:25 +0200

    fix: Use zero indexted columns properly on module insert

--- components/Settings/Modules.js
+++ components/Settings/Modules.js
@@ -80,7 +80,7 @@ export default function Modules({ settings, modules, setModules, moveModule }) {
               className="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-primary-500 focus:outline-none focus:ring-primary-500 dark:bg-gray-800 dark:text-white sm:text-sm"
             >
               {Array.from(Array(+settings.columns).keys()).map((column) => (
-                <option key={column} value={column + 1}>
+                <option key={column} value={column}>
                   {column + 1}
                 </option>
               ))}
