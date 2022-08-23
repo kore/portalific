5cd1963d9b3e2986ba2e59157ce73a024fa3c53e
Kore Nordmann <mail@kore-nordmann.de>
2022-08-23 09:17:04 +0200

    fix: Paths to static files

--- pages/_document.js
+++ pages/_document.js
@@ -9,26 +9,22 @@ class MyDocument extends Document {
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="/static/apple-touch-icon.png"
+            href="/apple-touch-icon.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="/static/favicon-32x32.png"
+            href="/favicon-32x32.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="/static/favicon-16x16.png"
+            href="/favicon-16x16.png"
           />
-          <link rel="manifest" href="/static/site.webmanifest" />
-          <link
-            rel="mask-icon"
-            href="/static/safari-pinned-tab.svg"
-            color="#4b4999"
-          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#4b4999" />
           <meta name="msapplication-TileColor" content="#603cba" />
           <meta name="theme-color" content="#4b4999" />
         </Head>
