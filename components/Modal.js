380f78228982d01fafbbce672c280a13eb2b0f62
Kore Nordmann <mail@kore-nordmann.de>
2025-03-26 13:58:51 +0100

    chore: Improved modal styling

--- components/Modal.js
+++ components/Modal.js
@@ -1,52 +1,24 @@
 import { Fragment } from "react";
-import { Dialog, Transition } from "@headlessui/react";
+import { Dialog } from "@headlessui/react";
 
 export default function Modal({ open, setOpen, children }) {
   return (
-    <Transition.Root show={open} as={Fragment}>
-      <Dialog as="div" className="modal" onClose={setOpen}>
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div className="fixed inset-0 bg-gray-500/75 transition-opacity" />
-        </Transition.Child>
-
-        <div className="fixed inset-0 z-10 overflow-y-auto">
-          <div className="flex min-h-full items-center justify-center p-4">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 translate-y-4 scale-95"
-              enterTo="opacity-100 translate-y-0 scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 translate-y-0 scale-100"
-              leaveTo="opacity-0 translate-y-4 scale-95"
-            >
-              <Dialog.Panel className="modal__content theme-transition">
-                <div className="modal__body">
-                  {children}
-                </div>
-
-                <div className="modal__footer">
-                  <button
-                    type="button"
-                    className="button button--primary"
-                    onClick={() => setOpen(false)}
-                  >
-                    Close
-                  </button>
-                </div>
-              </Dialog.Panel>
-            </Transition.Child>
+      <Dialog as="div" className="modal" open={open} onClose={setOpen}>
+        <Dialog.Panel className="modal__content">
+          <div className="modal__body">
+            {children}
           </div>
-        </div>
+
+          <div className="modal__footer">
+            <button
+              type="button"
+              className="button button--primary"
+              onClick={() => setOpen(false)}
+            >
+              Close
+            </button>
+          </div>
+        </Dialog.Panel>
       </Dialog>
-    </Transition.Root>
   );
 }
