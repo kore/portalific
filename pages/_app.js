db34506491b2a95bc6e45eef59f9afdb68cd0afe
Kore Nordmann <mail@kore-nordmann.de>
2025-03-28 09:40:13 +0100

    feat: Move theme specific styling into dedcaited file

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import "../styles/globals.scss";
-import "../styles/theme.scss";
+import "../styles/theme-default.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
