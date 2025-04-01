7f0b62dfee4b1ae8cdd22fa976480083fb3e3712
Kore Nordmann <mail@kore-nordmann.de>
2025-04-01 09:14:04 +0200

    chore: Use more common name for manifest

--- pages/_document.js
+++ pages/_document.js
@@ -23,7 +23,7 @@ class MyDocument extends Document {
             sizes="16x16"
             href="/favicon-16x16.png"
           />
-          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="manifest" href="/manifest.json" />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#748e63" />
           <meta name="msapplication-TileColor" content="#748e63" />
           <meta name="theme-color" content="#4b4999" />
