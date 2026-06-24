10e6b7020f53d2aab5278258ffc3a369d163d62c
Kore Nordmann <mail@kore-nordmann.de>
2026-06-24 10:42:47 +0200

    refactor(css): replace SCSS/BEM with plain CubeCSS layers
    Drop Sass and the BEM stylesheet for plain CSS organised in CubeCSS layers
    (global/composition/utilities/blocks/exceptions/print). Themes and dark mode
    become document-level data attributes ([data-theme]/[data-variant]). Remove the
    sass, classnames and @headlessui/react dependencies.

--- main.jsx
+++ main.jsx
@@ -2,10 +2,12 @@ import { createRoot } from 'react-dom/client'
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
 import App from './App'
-import './styles/globals.scss'
-import './styles/theme-default.scss'
-import './styles/theme-black.scss'
-import './styles/theme-green.scss'
+import './styles/global.css'
+import './styles/composition.css'
+import './styles/utilities.css'
+import './styles/blocks.css'
+import './styles/exceptions.css'
+import './styles/print.css'
 
 createRoot(document.getElementById('root')).render(
   <DndProvider backend={HTML5Backend}>
